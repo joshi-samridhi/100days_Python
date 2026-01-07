cebeb90ed40dc42716fde9110ce3788ddbb6b230
joshi-samridhi <samridhijoshi03@gmail.com>
2026-01-08 00:38:17 +0530

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,12 @@
+import random
+
+# random_integer= random.randint(1,10)
+# print(random_integer)
+
+# random_0_1 = random.random()
+# print(random_0_1)
+
+random_head_tails= random.randint(0,1)
+if random_head_tails == 0:
+    print("Heads")
+else: print("tails")
